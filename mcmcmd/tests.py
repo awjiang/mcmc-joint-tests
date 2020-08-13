8601ef391253043439f64ac1350b4cbcb428ef6b
awjiang <awjiang@gmail.com>
2020-08-13 12:35:53 +0100

    Graph kernel, linear time MMD, results

--- mcmcmd/tests.py
+++ mcmcmd/tests.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import numpy as onp
-import shogun as sg
+# import shogun as sg
 from matplotlib import pyplot as plt
 from scipy.stats import norm
 from scipy.stats import ks_2samp, chisquare, rankdata
@@ -19,16 +19,175 @@ def splitIter(num_iter, nproc):
         arr_iter[i] += 1
     return arr_iter
 
+#######################################################################
+############################# Kernels #################################
+#######################################################################
+
+class kernel(object):
+    def __init__(self, X, Y):
+        self._X = X
+        self._Y = Y
+        pass
+
+    @property
+    def params(self):
+        pass
+
+    def set_params(self, params):
+        pass
+    
+    def eval(self):
+        pass
+
+    def f_kernel(self):
+        pass
+
 ''' 
 Generate RBF kernel matrix with width `tau`. 
+If `tau` is None, will use the median heuristic.
 Each row corresponds to an observation (row) from X; each column corresponds to an observation (row) from Y.
 '''
-def rbf_kernel(X, Y, tau):
-    n_X, p = X.shape
-    n_Y, p_Y = Y.shape
-    assert p == p_Y
-    K = onp.exp(-((X.reshape(n_X, 1, p) - Y.reshape(1, n_Y, p))**2).sum(2) / tau)
-    return K
+class rbf_kernel(kernel):
+    def __init__(self, X, Y, tau=None):
+        assert X.shape[1] == Y.shape[1]
+        assert len(X.shape) == 2 and len(X.shape) == len(Y.shape)
+        if tau is not None:
+            assert isinstance(tau, int) or isinstance(tau, float)
+        self._X = X
+        self._Y = Y
+        self._tau = tau
+        pass
+
+    @property
+    def params(self):
+        return self._tau
+
+    def set_params(self, params):
+        self._tau = params
+        pass
+
+    def learn(self, method='median_heuristic', eval=False):
+        assert method in ['median_heuristic']
+        n_X, p = self._X.shape
+        n_Y = self._Y.shape[0]
+        if method == 'median_heuristic':
+            norm2 = ((self._X.reshape(n_X, 1, p) - self._Y.reshape(1, n_Y, p))**2).sum(2)
+            norm2_sorted = onp.sort(norm2.reshape(n_X*n_Y, 1).flatten())
+            if (n_X*n_Y) % 2 == 0:
+                tau = (norm2_sorted[int(n_X*n_Y/2)-1] + norm2_sorted[int(n_X*n_Y/2)])/2
+            else:
+                tau = norm2_sorted[int(n_X*n_Y/2)]
+        else:
+            raise ValueError
+        self._tau = tau
+
+        if eval == True:
+            return onp.exp(-norm2/self._tau)
+        else:
+            pass
+
+    def eval(self):
+        if self._tau is None:
+            K = self.learn(eval=True)
+        else:
+            n_X, p = self._X.shape
+            n_Y = self._Y.shape[0]
+            K = onp.exp(-(((self._X.reshape(n_X, 1, p) - self._Y.reshape(1, n_Y, p))**2).sum(2))/self._tau)
+        return K
+
+    def f_kernel(self, x, y, tau=None):
+        if tau is None:
+            tau = self._tau
+        assert len(x.shape) == len(y.shape) and len(x.shape) == 1
+        return onp.exp(-((x-y)**2).sum()/tau)
+
+class sum_uni_rbf_kernel(rbf_kernel):
+    def __init__(self, X, Y, tau=None):
+        assert X.shape[1] == Y.shape[1]
+        assert len(X.shape) == 2 and len(X.shape) == len(Y.shape)
+        if tau is not None:
+            if isinstance(tau, onp.ndarray):
+                tau = tau.reshape(1, 1, X.shape[1])
+            else:
+                assert isinstance(tau, int) or isinstance(tau, float)
+        self._X = X
+        self._Y = Y
+        self._tau = tau
+        pass
+
+    def learn(self, method='median_heuristic', eval=False):
+        n_X, p = self._X.shape
+        n_Y = self._Y.shape[0]
+        if method == 'median_heuristic':
+            norm2 = ((self._X.reshape(n_X, 1, p) - self._Y.reshape(1, n_Y, p))**2)
+            norm2_sorted = onp.sort(norm2.reshape(n_X*n_Y,p,order='F'), axis=0)
+            if (n_X*n_Y) % 2 == 0:
+                tau = (norm2_sorted[int(n_X*n_Y/2)-1, :] + norm2_sorted[int(n_X*n_Y/2), :])/2
+            else:
+                tau = norm2_sorted[int(n_X*n_Y/2), :]
+        tau = tau.reshape(1,1,p)
+        self._tau = tau
+        if eval == True:
+            return onp.exp(-norm2/self._tau).sum(2)
+        else:
+            pass        
+
+    def eval(self):
+        if self._tau is None:
+            K = self.learn(eval=True)
+        else:
+            n_X, p = self._X.shape
+            n_Y = self._Y.shape[0]
+            K = onp.exp(-((self._X.reshape(n_X, 1, p) - self._Y.reshape(1, n_Y, p))**2)/self._tau).sum(2)
+        return K
+
+    def f_kernel(self, x, y, tau=None):
+        if tau is None:
+            tau = self._tau
+        assert len(x.shape) == len(y.shape) and len(x.shape) == 1
+        return onp.exp(-((x-y)**2)/tau).sum()
+# ''' 
+# Generate RBF kernel matrix with width `tau`. 
+# If `tau` is None, will use the median heuristic.
+# Each row corresponds to an observation (row) from X; each column corresponds to an observation (row) from Y.
+# '''
+# def rbf_kernel(X, Y, tau=None):
+#     assert X.shape[1] == Y.shape[1] and len(X.shape) == len(Y.shape) and len(X.shape) == 2
+#     n_X, p = X.shape
+#     n_Y = Y.shape[0]
+
+#     norm2 = ((X.reshape(n_X, 1, p) - Y.reshape(1, n_Y, p))**2).sum(2)
+#     if tau is None:
+#         norm2_sorted = onp.sort(norm2.reshape(n_X*n_Y, 1).flatten())
+#         if (n_X*n_Y) % 2 == 0:
+#             tau = (norm2_sorted[int(n_X*n_Y/2)-1] + norm2_sorted[int(n_X*n_Y/2)])/2
+#         else:
+#             tau = norm2_sorted[int(n_X*n_Y/2)]
+
+#     K = onp.exp(-norm2/tau)
+#     return K
+
+# ''' 
+# Generate sum of univariate RBF kernel matrices with width `tau`.
+# If `tau` is None, will use the median heuristic.
+# Each row corresponds to an observation (row) from X; each column corresponds to an observation (row) from Y.
+# '''
+# def sum_rbf_kernel(X, Y, tau=None):
+#     assert X.shape[1] == Y.shape[1] and len(X.shape) == len(Y.shape) and len(X.shape) == 2
+#     n_X, p = X.shape
+#     n_Y = Y.shape[0]
+
+#     norm2 = ((X.reshape(n_X, 1, p) - Y.reshape(1, n_Y, p))**2)
+#     if tau is None:
+#         norm2_sorted = onp.sort(norm2.reshape(n_X*n_Y,p,order='F'), axis=0)
+#         if (n_X*n_Y) % 2 == 0:
+#             tau = (norm2_sorted[int(n_X*n_Y/2)-1, :] + norm2_sorted[int(n_X*n_Y/2), :])/2
+#         else:
+#             tau = norm2_sorted[int(n_X*n_Y/2), :]
+
+#     tau = tau.reshape(1,1,p)
+#     K = onp.exp(-norm2/tau).sum(2)
+#     return K
 
 #######################################################################
 ############################# Geweke test #############################
@@ -66,6 +225,7 @@ def rbf_kernel(X, Y, tau):
 
 '''
 Calculate the squared standard error of the estimate of E[`g`] (Geweke 1999, 3.7-8). If `L`=None, automatically selects bandwidth for the lag window based on an asymptotic MSE criterion (Andrews 1991). This assumes that `g` is fourth-moment stationary and the autocovariances are L1-summable.
+Note: depends on arch
 '''
 def geweke_se2(g, L=None, force_int_L=False):
     if len(g.shape) == 1:
@@ -159,49 +319,156 @@ def prob_plot(x, y, plot_type='PP', step = 0.005):
 #######################################################################
 
 '''
-Run MMD test on samples with shape (n x p)
+Run MMD test on samples with shape (n x p). Depends on shogun
 '''
-def mmd_test(samples_p, samples_q, lst_kernels, alpha=0.05, train_test_ratio=1, num_runs=1, num_folds=3):
-    if type(lst_kernels).__name__ != 'list':
-        lst_kernels = [lst_kernels]
-    if len(samples_p.shape) == 1:
-        samples_p = samples_p.reshape(samples_p.shape[0], 1)
-    if len(samples_q.shape) == 1:
-        samples_q = samples_q.reshape(samples_q.shape[0], 1)
+# def mmd_test(samples_p, samples_q, lst_kernels, alpha=0.05, train_test_ratio=1, num_runs=1, num_folds=3):
+#     if type(lst_kernels).__name__ != 'list':
+#         lst_kernels = [lst_kernels]
+#     if len(samples_p.shape) == 1:
+#         samples_p = samples_p.reshape(samples_p.shape[0], 1)
+#     if len(samples_q.shape) == 1:
+#         samples_q = samples_q.reshape(samples_q.shape[0], 1)
     
-    assert samples_p.shape[1] == samples_q.shape[1]
+#     assert samples_p.shape[1] == samples_q.shape[1]
 
-    features_p = sg.RealFeatures(samples_p.T)
-    features_q = sg.RealFeatures(samples_q.T)
+#     features_p = sg.RealFeatures(samples_p.T)
+#     features_q = sg.RealFeatures(samples_q.T)
 
-    if samples_p.shape[0] == samples_q.shape[0]:
-        mmd = sg.LinearTimeMMD(features_p, features_q)
+#     if samples_p.shape[0] == samples_q.shape[0]:
+#         mmd = sg.LinearTimeMMD(features_p, features_q)
+#     else:
+#         mmd = sg.QuadraticTimeMMD(features_p, features_q)
+
+#     mmd.set_statistic_type(sg.ST_UNBIASED_FULL) # V-statistic = ST_BIASED_FULL
+    
+#     # if multiple kernels are provided, will learn which maximizes test power and compute the result on a held-out test set
+#     if len(lst_kernels) > 1:
+#         mmd.set_train_test_mode(True)
+#         mmd.set_train_test_ratio(train_test_ratio)
+#         for k in lst_kernels:
+#             mmd.add_kernel(k)
+#         mmd.set_kernel_selection_strategy(sg.KSM_MAXIMIZE_POWER, num_runs, num_folds, alpha)
+#         mmd.select_kernel()
+#         learnt_kernel_single = sg.GaussianKernel.obtain_from_generic(mmd.get_kernel())
+#         width = learnt_kernel_single.get_width()
+#     elif len(lst_kernels) == 1:
+#         mmd.set_train_test_mode(False)
+#         mmd.set_kernel(lst_kernels[0])
+#         width = lst_kernels[0].get_width()
+
+#     test_statistic = mmd.compute_statistic()
+#     p_value = mmd.compute_p_value(test_statistic)
+#     threshold = mmd.compute_threshold(alpha)
+#     result = p_value <= alpha
+#     return {'result': result, 'p_value': p_value, 'test_statistic': test_statistic, 'critical_value': threshold, 'kernel_width':width}
+
+'''
+Quadratic/Linear time MMD test. No shogun dependency, but slow
+'''
+def mmd_test(X, Y, kernel=rbf_kernel, alpha=0.05, null_samples=100, kernel_learn_method='median_heuristic', mmd_type='unbiased', rng=None, **kwargs):
+    assert X.shape[1] == Y.shape[1] and len(X.shape) == 2 and len(Y.shape) == 2
+    assert mmd_type in ['biased', 'unbiased', 'linear']
+    assert kernel_learn_method is None or kernel_learn_method in ['median_heuristic']
+    if rng is None:
+        rng = onp.random.default_rng()
+
+    XY = onp.vstack([X,Y])
+    K = kernel(XY, XY, **kwargs)
+
+    if kernel_learn_method == 'median_heuristic':
+        assert kernel == rbf_kernel
+        K_learn = kernel(X, Y)
+        K_learn.learn()
+        kernel_param = K_learn.params
+        K.set_params(kernel_param)
+
+    if mmd_type == 'linear':
+        assert X.shape == Y.shape
+        n, p = X.shape
+        f_kernel = K.f_kernel
+
+        # Calculate test statistic
+        test_statistic, var = mmd_l(X, Y, f_kernel, return_2nd_moment=True)
+        var -= test_statistic**2
+        scale = onp.sqrt(2.*var/n)
+        p_value = norm.sf(test_statistic, scale=scale)
+        result = p_value <= alpha
+        threshold = norm.ppf(1.-alpha, scale=scale)
     else:
-        mmd = sg.QuadraticTimeMMD(features_p, features_q)
+        
+        # Calculate null distribution       
+        K_XYXY = K.eval()
+        n_X, p = X.shape
+        n_Y = Y.shape[0]
+        null_distr = onp.zeros(null_samples)
+        for i in range(null_samples):
+            ind = rng.permutation(int(n_X+n_Y))
+            ind_X = ind[:n_X]
+            ind_Y = ind[n_Y:]
 
-    mmd.set_statistic_type(sg.ST_UNBIASED_FULL) # V-statistic = ST_BIASED_FULL
+            K_XX = K_XYXY[ind_X, :][:, ind_X]
+            K_YY = K_XYXY[ind_Y, :][:, ind_Y]
+            K_XY = K_XYXY[ind_X, :][:, ind_Y]
+            
+            if mmd_type == 'unbiased':
+                null_distr[i] = mmd_u(K_XX=K_XX, K_YY=K_YY, K_XY=K_XY)
+            elif mmd_type == 'biased':
+                null_distr[i] = mmd_v(K_XX=K_XX, K_YY=K_YY, K_XY=K_XY)
+
+        # Calculate test statistic
+        K_XX = K_XYXY[:n_X, :][:, :n_X]
+        K_YY = K_XYXY[n_X:, :][:, n_X:]
+        K_XY = K_XYXY[:n_X, :][:, n_X:]
+
+        if mmd_type == 'unbiased':
+            test_statistic = mmd_u(K_XX=K_XX, K_YY=K_YY, K_XY=K_XY)
+        elif mmd_type == 'biased':
+            test_statistic = mmd_v(K_XX=K_XX, K_YY=K_YY, K_XY=K_XY)    
+
+        threshold = onp.quantile(null_distr, 1.-alpha)
+        result = test_statistic >= threshold
+        p_value = (null_distr >= test_statistic).mean()
     
-    # if multiple kernels are provided, will learn which maximizes test power and compute the result on a held-out test set
-    if len(lst_kernels) > 1:
-        mmd.set_train_test_mode(True)
-        mmd.set_train_test_ratio(train_test_ratio)
-        for k in lst_kernels:
-            mmd.add_kernel(k)
-        mmd.set_kernel_selection_strategy(sg.KSM_MAXIMIZE_POWER, num_runs, num_folds, alpha)
-        mmd.select_kernel()
-        learnt_kernel_single = sg.GaussianKernel.obtain_from_generic(mmd.get_kernel())
-        width = learnt_kernel_single.get_width()
-    elif len(lst_kernels) == 1:
-        mmd.set_train_test_mode(False)
-        mmd.set_kernel(lst_kernels[0])
-        width = lst_kernels[0].get_width()
+    return {'result':result, 'p_value':p_value, 'test_statistic':test_statistic, 'critical_value':threshold, 'kernel_param':K.params}
 
-    test_statistic = mmd.compute_statistic()
-    p_value = mmd.compute_p_value(test_statistic)
-    threshold = mmd.compute_threshold(alpha)
-    result = p_value <= alpha
-    return {'result': result, 'p_value': p_value, 'test_statistic': test_statistic, 'critical_value': threshold, 'kernel_width':width}
+'''
+Generate (squared) Quadratic Time MMD u-statistic
+'''
+def mmd_u(K_XX, K_YY, K_XY, normalize=True):
+    assert K_XY.shape[0] == K_XY.shape[1]
+    m, n = K_XY.shape
+    if normalize == True:
+        if m == n:
+            z = n
+        else:
+            z = m * n / (m + n)
+    else:
+        z = 1.
+    return z*(1./(m*(m-1)) * (K_XX.sum() - onp.diag(K_XX).sum()) + 1./(n*(n-1)) * (K_YY.sum() - onp.diag(K_YY).sum()) - 2.*K_XY.mean())
 
+'''
+Generate (squared) Linear Time MMD u-statistic
+'''
+def mmd_l(X, Y, f_kernel, return_2nd_moment=False):
+    assert X.shape == Y.shape
+    n, p = X.shape
+
+    h = lambda x_i, y_i, x_j, y_j: f_kernel(x_i, x_j) + f_kernel(y_i, y_j) - f_kernel(x_i, y_j) - f_kernel(x_j, y_i)
+
+    n_2 = int(n/2)
+    stat = 0
+    second = 0
+    for i in range(n_2):
+        h_i = h(x_i=X[2*i, :], y_i=Y[2*i, :], x_j=X[2*i+1, :], y_j=Y[2*i+1, :])
+        stat += h_i
+        second += h_i**2
+    stat /= n_2
+    second /= n_2
+
+    if return_2nd_moment == True:
+        return stat, second
+    else:
+        return stat
 
 #######################################################################
 ############################ Wild MMD test ############################
@@ -260,42 +527,40 @@ def mmd_v(K_XX, K_YY, K_XY, normalize=True):
 '''
 Run Wild MMD test on samples with shape (n x p)
 '''
-def mmd_wb_test(X, Y, f_kernel, alpha=0.05, null_samples=200, wb_l_n=20, wb_center=True, rng=None):
+def mmd_wb_test(X, Y, kernel=rbf_kernel, alpha=0.05, null_samples=100, kernel_learn_method='median_heuristic', wb_l_n=20, wb_center=True, rng=None, **kwargs):
     if len(X.shape) == 1:
         X = X.reshape(X.shape[0], 1)
     if len(Y.shape) == 1:
         Y = Y.reshape(Y.shape[0], 1)
     if rng is None:
         rng = onp.random.default_rng()
-    
-    K_XX = f_kernel(X, X)
-    K_YY = f_kernel(Y, Y)
-    K_XY = f_kernel(X, Y)
-    
+
+    K_XX = kernel(X, X, **kwargs)
+    K_YY = kernel(Y, Y, **kwargs)
+    K_XY = kernel(X, Y, **kwargs)
+    if kernel_learn_method == 'median_heuristic':
+        assert kernel == rbf_kernel
+        K_learn = kernel(X, Y)
+        K_learn.learn()
+        kernel_param = K_learn.params
+        K_XX.set_params(kernel_param)
+        K_YY.set_params(kernel_param)
+        K_XY.set_params(kernel_param)
+
+    K_XX_eval = K_XX.eval()
+    K_YY_eval = K_YY.eval()
+    K_XY_eval = K_XY.eval()
+
     B = onp.empty(null_samples)
     for i in range(null_samples):
-        B[i] = mmd_wb(K_XX, K_YY, K_XY, normalize=True, wb_l_n=wb_l_n, wb_center=wb_center, rng=rng)
+        B[i] = mmd_wb(K_XX_eval, K_YY_eval, K_XY_eval, normalize=True, wb_l_n=wb_l_n, wb_center=wb_center, rng=rng)
 
     threshold = onp.quantile(B, 1.-alpha)
-    test_statistic = mmd_v(K_XX, K_YY, K_XY)
+    test_statistic = mmd_v(K_XX_eval, K_YY_eval, K_XY_eval, normalize=True)
     result = test_statistic >= threshold
     p_value = (B >= test_statistic).mean() # one-sided
     
-    return {'result':result, 'p_value':p_value, 'test_statistic':test_statistic, 'critical_value':threshold}
-
-
-'''
-Generate (squared) MMD u-statistic for the Wild MMD test
-'''
-def mmd_u(K_XX, K_YY, K_XY, normalize=True):
-    assert K_XY.shape[0] == K_XY.shape[1]
-    n = K_XY.shape[0]
-    if normalize == True:
-        z = n
-    else:
-        z = 1.
-    return z*(1./(n*(n-1)) * (K_XX.sum() - onp.diag(K_XX).sum()) + 1./(n*(n-1)) * (K_YY.sum() - onp.diag(K_YY).sum()) - 2.*K_XY.mean())
-
+    return {'result':result, 'p_value':p_value, 'test_statistic':test_statistic, 'critical_value':threshold, 'kernel_param':K_XX.params}
 
 '''
 Estimate MMD variance
@@ -458,27 +723,28 @@ def rank_test(model, N, L, alpha=0.05, rng=None):
 
 # Sequential wrapper
 def f_test_sequential(sample_size, model, test_type, **kwargs):
-    assert test_type in ['rank', 'ks', 'mmd', 'mmd-wb', 'geweke']
+    assert test_type in ['rank', 'ks', 'mmd', 'mmd_sum_uni', 'mmd-wb', 'geweke']
         
     if test_type == 'rank':
         p_values = rank_test(model, N=500, L=5)['p_value']
-    elif test_type in ['ks', 'mmd']:
+    elif test_type in ['ks', 'mmd', 'mmd_sum_uni']:
         X = model.test_functions(model.sample_mc(sample_size))
         Y = model.test_functions(model.sample_bc(sample_size, burn_in_samples=5))
         if test_type == 'ks':
             p_values = ks_test(X, Y)['p_value']
         elif test_type == 'mmd':
-            p_values = mmd_test(X, Y, sg.GaussianKernel(64, tau=1))['p_value']
+            p_values = mmd_test(X, Y, kernel=rbf_kernel, mmd_type='unbiased')['p_value']
+        elif test_type == 'mmd_sum_uni':
+            p_values = mmd_test(X, Y, kernel=sum_uni_rbf_kernel, mmd_type='unbiased')['p_value']
     elif test_type in ['mmd-wb', 'geweke']:
         if test_type == 'mmd-wb':
-            mmd_test_size = 500
-            mmd_thinning = onp.arange(0, int(sample_size), 1)#int(sample_size/mmd_test_size))
+            mmd_test_size = int(sample_size)
+            mmd_thinning = onp.arange(0, int(sample_size), 1) #int(sample_size/mmd_test_size)
             X = model.test_functions(model.sample_mc(mmd_test_size))
             Y = model.test_functions(model.sample_sc(sample_size))
-            f_kernel = lambda X, Y: rbf_kernel(X, Y, tau=1)
-            p_values = mmd_wb_test(X, Y[mmd_thinning, :], f_kernel)['p_value']
+            p_values = mmd_wb_test(X, Y[mmd_thinning, :])['p_value']
         elif test_type == 'geweke':
-            geweke_thinning = onp.arange(0, int(sample_size), 50)
+            geweke_thinning = onp.arange(0, int(sample_size), 1)
             X = model.test_functions(model.sample_mc(sample_size))
             Y = model.test_functions(model.sample_sc(sample_size))
             p_values = geweke_test(X, Y[geweke_thinning, :], l=0.15, test_correction='b')['p_value']        
